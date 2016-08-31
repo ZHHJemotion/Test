25818540cac6cf7ad33d91cfb64990ad026467d2
zhhjemotion <zhhjemotoin@hotmail.com>
2016-08-31 21:18:10 +0200

    lukas zhang

---
+++ HelloProject/HelloProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloProject
+//
+//  Created by 张行剑 on 16/8/31.
+//  Copyright © 2016年 Lukas_Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
