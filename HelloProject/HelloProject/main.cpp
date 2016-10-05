7452894fd149ea7b4e05a4369013d4a9720046b4
zhhjemotion <zhhjemotoin@hotmail.com>
2016-10-05 11:42:11 +0200

    opencv_test1

--- HelloProject/HelloProject/main.cpp
+++ HelloProject/HelloProject/main.cpp
@@ -7,6 +7,11 @@
 //
 
 #include <iostream>
+#include <opencv2/core/core.hpp>
+#include<opencv2/highgui/highgui.hpp>
+
+using namespace cv;
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
